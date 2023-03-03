32532ebfaa130b82f3861efb25b1dcc438714996
Sanket Kailkar <sanketkailkar.14@gmail.com>
2023-03-03 13:29:29 +0530

    Update

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var btnTranslate = document.querySelector(".translate-btn");
+var btnTranslate = document.querySelector("#translate-btn");
 var txtInput = document.querySelector("#translate-input");
 var output = document.querySelector("#translate-output");
 
-// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" //tanaypratap
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
@@ -16,10 +16,11 @@ function errorHandler(error){
 }
 
 function clickHandler(){
-    var inputText = txtInput.value; //taking input
+    var inputText = txtInput.value;
 
     // calling server for processing 
     fetch(getTranslationURL(inputText))
+
         .then(response => response.json())
         .then(json => {
         var translatedText = json.contents.translated;
